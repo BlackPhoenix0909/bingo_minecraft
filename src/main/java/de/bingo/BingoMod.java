0012b7f3ef7789ceeda748912f89129a73014855
BlackPhoenix0909 <juliuscguhl@icloud.com>
2026-05-28 11:52:22 +0200

    Update BingoMod.java

--- src/main/java/de/bingo/BingoMod.java
+++ src/main/java/de/bingo/BingoMod.java
@@ -8,7 +8,7 @@ import net.minecraft.server.command.CommandManager;
 import net.minecraft.text.Text;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+ 
 public class BingoMod implements ModInitializer {
     public static final String MOD_ID = "bingo";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
